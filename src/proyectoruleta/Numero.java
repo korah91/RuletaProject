ec3389b48aece9fe5724aadc264a516a4ab6b9d6
duraan08 <62382060+duraan08@users.noreply.github.com>
2020-03-23 12:56:58 +0100

    Update Numero.java

--- src/proyectoruleta/Numero.java
+++ src/proyectoruleta/Numero.java
@@ -4,20 +4,22 @@ public class Numero {
 	//atributos
 
 	private String color; 
-	private int numero;
-	private boolean par;
-	private int docena; 
-	private int mitad; 
+	private int  numero;
+	private String par;
+	private String docena; 
+	private String  mitad; 
+	private String familia; 
 	
 	//Constructoras
 
-	public Numero(String pColor, int pNumero, boolean pPar, int pDocena, int pMitad) {
+	public Numero(String pColor, int pNumero, String pPar, String pDocena, String pMitad, String pFamilia) {
 		
 		this.color = pColor; 
 		this.numero = pNumero; 
 		this.par = pPar; 
 		this.docena = pDocena; 
 		this.mitad = pMitad; 
+		this.familia = pFamilia; 
 	}
 	
 	//metodos adicionales
@@ -27,9 +29,27 @@ public class Numero {
 		return(this.numero);
 	}
 	
-	protected double getPremio() {
-		double premio = 0.0;
+	public String getColor() {
+		return(this.color); 
+	}
+	
+	public String getPar() { 
 		
-		return(premio);
+		return(this.par); 
+	}
+	
+	public String getDocena() {
+		
+		return(this.docena); 
+	}
+	
+	public String getMitad() {
+		
+		return(this.mitad); 
+	}
+	
+	public String getFamilia() {
+		
+		return(this.familia); 
 	}
 }
