2caa8992f9f306fa4fa79af97766688b11f433b0
duraan08 <62382060+duraan08@users.noreply.github.com>
2020-03-23 12:57:42 +0100

    Update ListaNumeros.java

--- src/proyectoruleta/ListaNumeros.java
+++ src/proyectoruleta/ListaNumeros.java
@@ -15,7 +15,6 @@ public class ListaNumeros {
 	}
 	
 	//metodos adicionales
-	
 	private Iterator<Numero> getIterador(){
 		
 		return(lista.iterator());
@@ -29,7 +28,7 @@ public class ListaNumeros {
 		while (itr.hasNext() && !encontrado){
 			numeroPremiado = itr.next();
 			
-			if(numeroPremiado.getNumero() == pNum){
+			if(numeroPremiado.getNumero() == pNum ){
 				encontrado = true;
 			}
 		}
@@ -47,67 +46,7 @@ public class ListaNumeros {
         
         return (numeroPremiado);
     }
+	
+	
 
-	
-	public ArrayList<Numero> guardarNumerosPremiados() {
-		
-		ArrayList<Numero> listaNumerosPremiados = new ArrayList<Numero>();
-		Numero numeroPremiado = null; 
-		
-		numeroPremiado = this.lanzarBola();
-		listaNumerosPremiados.add(numeroPremiado); 
-		
-		return(listaNumerosPremiados);
-	}
-	
-	public int cuantosNumerosPremiados() {
-		ArrayList<Numero> listaPremiados = this.guardarNumerosPremiados();
-		
-		return(listaPremiados.size());
-	}
-	
-	public void imprimirTiradas() {
-		
-		ArrayList<Numero> listaPremiados = this.guardarNumerosPremiados(); 
-		int indice = listaPremiados.size();
-		int contador = 0;
-		
-		System.out.println("Los ultimos 5 numeros premiados han sido : ");
-		
-		while (contador != 5 ) {
-			
-			System.out.println( listaPremiados.get(indice) );
-			indice = indice - 1; 
-			contador = contador + 1; 
-		}
-	}
-	
-	public void  imprimirCalientesYFrios() {
-		ArrayList<Numero> listaDePremiados = this.guardarNumerosPremiados();
-		ArrayList<Numero> listaCalientes = new ArrayList<Numero>();
-		ArrayList<Numero> listaFrios = new ArrayList<Numero>();
-		int indice = 0; 
-		int contador = 0;
-		
-		while (indice < 36) {
-			contador = Collections.frequency(listaDePremiados, indice);
-			indice = indice + 1; 
-		}
-		
-		if (contador >= 3 && listaCalientes.contains(indice)) {
-			listaCalientes.add(listaDePremiados.get(indice));
-		}
-		else {
-			if(!listaFrios.contains(indice)) {
-			listaFrios.add(listaDePremiados.get(indice));
-		
-			}
-		}	
-	}
-
-	public double getPremio() {
-		double premio = 0.0;
-		
-		return(premio);
-	}
 }
