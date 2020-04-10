12e2e19d035e5775c997c8cc8f5882d34ba5205b
duraan08 <62382060+duraan08@users.noreply.github.com>
2020-04-10 12:10:32 +0200

    Update Apuesta.java

--- src/proyectoruleta/Apuesta.java
+++ src/proyectoruleta/Apuesta.java
@@ -2,12 +2,19 @@ package proyectoruleta;
 
 public abstract class Apuesta {
 	
-	protected double cantidad;
-	protected double multiplicador;
+	private double cantidad;
 	
 	public Apuesta(double pCantidad) {
 		this.cantidad = pCantidad;
 	}
 	
 	public abstract double getPremio(Numero pNumero);
+	
+	protected double getCantidad() {
+	
+		return this.cantidad; 
+	}
+	
+	
 }
+
